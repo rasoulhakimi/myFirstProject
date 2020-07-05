3de2de3460de3947d0f5bbfb64580f1d84f4499c
rasoul <rasoulhakimi@gmail.com>
2020-07-05 16:39:09 +0430

    Updates

---
+++ precache-manifest.52b83aa157666222042b90703e6f0894.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03cb634ffbe360d40fbba431df1d052",
+    "url": "/myFirstProject/index.html"
+  },
+  {
+    "revision": "114cda891eb10d36ae39",
+    "url": "/myFirstProject/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c4f511e9db43c2daea89",
+    "url": "/myFirstProject/static/css/main.ba862fa7.chunk.css"
+  },
+  {
+    "revision": "114cda891eb10d36ae39",
+    "url": "/myFirstProject/static/js/2.6c2f029a.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/myFirstProject/static/js/2.6c2f029a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f511e9db43c2daea89",
+    "url": "/myFirstProject/static/js/main.466d9f0f.chunk.js"
+  },
+  {
+    "revision": "6d6663c786156ac64a85",
+    "url": "/myFirstProject/static/js/runtime-main.c98df45d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myFirstProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
